9264b6df642c32025b733078303db30b1f7b16b4
meggouri ismail <meggouri.ismail@gmail.com>
2021-05-27 17:39:11 +0100

    messing things up

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -4,7 +4,7 @@ const Education = () => {
             <h1 className="title">Education</h1>
             <div className="bloc">
                 <div className="bac-bloc">
-                    <img src="../assets/icons/medal_96px.png" alt="diploma" />
+                    <img src="%PUBLIC_URL%/assets/icons/medal_96px.png" alt="diploma" />
                     <p>Baccaloriat</p>
                     <p>2017 / 2018</p>
                 </div>

4056022a54d67a1a514d33a47eb3183f0c26bec9
meggouri ismail <meggouri.ismail@gmail.com>
2021-05-27 16:31:45 +0100

    add emailjs to send emails

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,23 +1,41 @@
+import emailjs from 'emailjs-com';
+
 const Contact = () => {
+
+    const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+    const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+    const USER_ID = process.env.REACT_APP_USER_ID;
+
+    function sendEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    }
+
     return (
         <section className="form-section" id="contact">
             <h1 className="title">Contact Me</h1>
             <div className="form-bloc">
-                <form>
+                <form onSubmit={sendEmail}>
                     <div className="contact-info">
-                        <input type="text" className="form__input" placeholder=" " />
+                        <input type="text" className="form__input" placeholder=" " name="name" />
                         <label htmlFor="" className="form__label">your name</label>
                     </div>
                     <div className="contact-info">
-                        <input type="text" className="form__input" placeholder=" " />
+                        <input type="text" className="form__input" placeholder=" " name="email" />
                         <label htmlFor="" className="form__label">your email</label>
                     </div>
                     <div className="subject">
-                        <input type="text" className="form__input" placeholder=" " />
-                        <label htmlFor="" className="form__label">object</label>
+                        <input type="text" className="form__input" placeholder=" " name="subject" />
+                        <label htmlFor="" className="form__label">subject</label>
                     </div>
                     <div className="contact-msg">
-                        <textarea className="form__textarea" placeholder=" "></textarea>
+                        <textarea className="form__textarea" placeholder=" " name="message"></textarea>
                         <label htmlFor="" className="form__label1">message</label>
                     </div>
                     <button type="submit" value="Send" className="send-btn" >

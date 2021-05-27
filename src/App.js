2f6a28ca015f2fb2164fd6c83ebfc76d8dc7cffc
meggouri ismail <meggouri.ismail@gmail.com>
2021-05-27 17:45:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Contact from './Components/Contact';
 import Education from './Components/Education';
 import Header from './Components/Header';
@@ -7,13 +6,13 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <Education />
       <Portfolio />
       <Contact />
       <Footer />
-    </div>
+    </>
   );
 }
 
